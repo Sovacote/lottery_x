8aadcfc276d1aa2b250110c6d1f2126c5ca8041c
Sovacote <160414655+Sovacote@users.noreply.github.com>
2026-08-17 00:50:57 +0700

    Delete src/main/java/com directory

--- src/main/java/com/lottery/infrastructure/persistence/jdbc/JdbcConnectionProvider.java
+++
@@ -1,8 +0,0 @@
-package com.lottery.infrastructure.persistence.jdbc;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface JdbcConnectionProvider {
-    Connection currentConnection() throws SQLException;
-}
--- src/main/java/com/lottery/presentation/middleware/RequestContext.java
+++
@@ -1,5 +0,0 @@
-package com.lottery.presentation.middleware;
-
-public record RequestContext(String requestId, String correlationId, long startedAtNanos) {
-    public static final String ATTRIBUTE_NAME = RequestContext.class.getName();
-}
